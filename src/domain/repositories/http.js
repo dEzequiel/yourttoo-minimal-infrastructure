a09cad1c01e245d3c74cc337a35989ccf899829a
eze <linezekdlr@hotmail.com>
2024-10-22 13:34:54 +0200

    Repository with JSDoc documentation implementation.

---
+++ src/domain/repositories/http.js
@@ -0,0 +1,7 @@
+/**
+ * @typedef {Object} Http
+ * @property {function(string, Record<string, any>=, any=): Promise<any>} get
+ * @property {function(string, Record<string, any>=, any=): Promise<any>} post
+ * @property {function(string, Record<string, any>=, any=): Promise<any>} put
+ * @property {function(string, any=, any=): Promise<any>} delete
+ */
---
+++ src/infrastructure/repositories/helldiver.repository.js
@@ -0,0 +1,34 @@
+import HelldiverDTO from "../http/dto/HelldiverDTO.js";
+import '../../domain/repositories/http.js';
+
+/**
+ * @typedef {Object} HelldiverRepository
+ * @property {function(): Promise<HelldiverDTO[]>} getHelldivers
+ * @property {function(): Promise<HelldiverDTO>} createHelldiver
+ */
+
+/**
+ * @param {Http} client
+ * @returns {HelldiverRepository}
+ */
+const HelldiverRepository = function (client) {
+    return {
+        /**
+         * @returns {Promise<HelldiverDTO[]>}
+         */
+        getHelldivers: async () => {
+            const response = await client.get()
+            return response.data.map(helldiver => new HelldiverDTO(helldiver));
+        },
+        
+        /**
+         * @returns {Promise<HelldiverDTO>}
+         */
+        createHelldiver: async () => {
+            const response = await client.post('/helldivers');
+            return new HelldiverDTO(response.data);
+        },
+    };
+};
+
+export default HelldiverRepository;
